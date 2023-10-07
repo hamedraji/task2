d373f37bca4621efda55bd144704f7619389d6bc
hamedraji <147277912+hamedraji@users.noreply.github.com>
2023-10-07 23:23:09 +0330

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,11 @@
+let numList=[0,0,0,0,0,0,0,0,0,0]
+
+let dice=Math.floor((Math.random()*5.1)+1)
+document.write(dice)
+
+
+if(dice===6){
+    numList[0]=1;{
+        document.write(numList)
+    }
+}
